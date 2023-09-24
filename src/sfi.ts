d79fa131e227a27f5af123349c6b98fb0c8e5ba5
Nico Schett <nicoschett@icloud.com>
2023-09-24 16:24:01 +0200

    WIP: add PRESHARED_KEY auth for tokenCreate

--- src/sfi.ts
+++ src/sfi.ts
@@ -1,4 +1,13 @@
-import { defineService } from "@snek-at/function";
+import {
+  ServiceError,
+  decorator,
+  defineService,
+  withContext,
+} from "@snek-at/function";
+
+import dotenv from "dotenv";
+
+dotenv.config();
 
 import { TokenFactory, TokenFactoryOptions } from "./utils/TokenFactory";
 
@@ -20,24 +29,47 @@ export default defineService({
     },
   },
   Mutation: {
-    tokenCreate: (
-      userId: string,
-      resourceId: string,
-      scope: {
-        [key: string]: string[];
-      },
-      roles?: string[],
-      factoryOptions?: TokenFactoryOptions
-    ) => {
-      const factory = new TokenFactory(factoryOptions);
+    tokenCreate: withContext(
+      () =>
+        (
+          userId: string,
+          resourceId: string,
+          scope: {
+            [key: string]: string[];
+          },
+          roles?: string[],
+          factoryOptions?: TokenFactoryOptions
+        ) => {
+          const factory = new TokenFactory(factoryOptions);
 
-      const tokenPair = factory.createTokenPair(userId, resourceId, {
-        scope,
-        roles,
-      });
+          const tokenPair = factory.createTokenPair(userId, resourceId, {
+            scope,
+            roles,
+          });
 
-      return tokenPair;
-    },
+          return tokenPair;
+        },
+      {
+        decorators: [
+          decorator(async (context, [test]: [test: string]) => {
+            if (process.env.PRESHARED_KEY) {
+              // Check for pre-shared key to allow token creation
+              if (
+                context.req.headers.authorization !== process.env.PRESHARED_KEY
+              ) {
+                throw new ServiceError("Unauthorized", {
+                  code: "UNAUTHORIZED",
+                  statusCode: 401,
+                  message: "You are not authorized to create tokens.",
+                });
+              }
+            }
+
+            return context;
+          }) as any,
+        ],
+      }
+    ),
     tokenRefresh: (
       accessToken: string,
       refreshToken: string,
